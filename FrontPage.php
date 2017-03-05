4ce8c853d323ea418a7dfd7fb3d62dbcced69951
ZenOokami <zaneblalock@gmail.com>
2017-03-05 02:32:26 -0500

    Pushing some slight changes after running tests (mainly to move to laptop device to edit on the go) -- Found out that you cannot affect the page's elements with php without javascrirpt, so going to load data across various pages - then check with if statements.

---
+++ FrontPage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+<body>
+	<h1 style="text-align: center;">
+		Mystery Game Noir
+	</h1>
+
+	<br><br>
+
+	<div style="text-align: center;">
+		<a class="button" href="/story/Mystery.php">Enter the Game</a>
+	</div>
+</body>
+
+<footer>
+	
+</footer>
+
+</html>
---
+++ story/Start.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+<body>
+	<div>
+
+	</div>
+
+	<div>
+		<?php
+		// Let's set some variables
+		$story_progression = 0;
+		$detective_name;
+
+		?>
+
+		<b>Hello Detective, can you give us your name?:</b><br>
+		&nbsp;
+
+		<form>
+			<input type="text" name="detective_name" placeholder="Please input your name">
+			<input type="submit">
+		</form>
+
+	</div>
+
+	<div>
+
+	</div>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
